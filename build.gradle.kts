e0a3ef6ad9f8a3c37f712dd87c63585498c10de4
michal caduri <michal.caduri@apiiro.com>
2024-12-29 16:46:08 +0200

    deleted slf4j

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,6 @@ repositories {
     mavenCentral()
 }
 
-
-dependencies {
-    implementation("org.slf4j:slf4j-api:2.0.9")
-    implementation("org.slf4j:slf4j-simple:2.0.9") // Simple backend for demonstration
-    testImplementation(kotlin("test"))
-}
-
 application {
     mainClass.set("MainKt")
 }
